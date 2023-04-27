158dfbedae211148665e098d7daa33b746b48abc
Beatriz Sousa <bpds200404@gmail.com>
2023-04-27 16:58:32 +0100

    Update semáforo.py

--- semáforo.py
+++ semáforo.py
@@ -1,40 +1,40 @@
-tabuleiro = ["", "", "",
-             "", "", "",
-             "", "", ""]
-jogador_atual = "V"
+tabuleiro = ["", "", "", "",
+             "", "", "", "",
+             "", "", "", ""]
+jogador_atual = "O"
 
 def exibir_tabuleiro():
-    print(tabuleiro[0] + " | " + tabuleiro[1] + " | " + tabuleiro[2])
+    print(tabuleiro[0] + " | " + tabuleiro[1] + " | " + tabuleiro[2] + " | " + tabuleiro[3])
     print("--+---+--")
-    print(tabuleiro[3] + " | " + tabuleiro[4] + " | " + tabuleiro[5])
+    print(tabuleiro[4] + " | " + tabuleiro[5] + " | " + tabuleiro[6] + " | " + tabuleiro[7])
     print("--+---+--")
-    print(tabuleiro[6] + " | " + tabuleiro[7] + " | " + tabuleiro[8])
+    print(tabuleiro[8] + " | " + tabuleiro[9] + " | " + tabuleiro[10] + " | " + tabuleiro[11])
 
 def verificar_fim_de_jogo():
     # Verificar linhas horizontais
-    for i in range(0, 9, 3):
+    for i in range(0, 11, 4):
         if tabuleiro[i] == tabuleiro[i+1] == tabuleiro[i+2] and tabuleiro[i] != "":
             return True
     # Verificar linhas verticais
-    for i in range(0, 3):
+    for i in range(0, 4):
         if tabuleiro[i] == tabuleiro[i+3] == tabuleiro[i+6] and tabuleiro[i] != "":
             return True
     # Verificar diagonais
-    if tabuleiro[0] == tabuleiro[4] == tabuleiro[8] and tabuleiro[0] != "":
+    if tabuleiro[0] == tabuleiro[5] == tabuleiro[9] and tabuleiro[0] != "":
         return True
-    if tabuleiro[2] == tabuleiro[4] == tabuleiro[6] and tabuleiro[2] != "":
+    if tabuleiro[3] == tabuleiro[5] == tabuleiro[7] and tabuleiro[3] != "":
         return True
     return False
 
 while not verificar_fim_de_jogo() and "" in tabuleiro:
     exibir_tabuleiro()
-    celula = int(input("Digite a célula para colocar o sinal (de 1 a 9): ")) - 1
+    celula = int(input("Digite a célula para colocar o sinal (de 1 a 11): ")) - 1
     if tabuleiro[celula] == "":
         tabuleiro[celula] = jogador_atual
-        if jogador_atual == "V":
-            jogador_atual = "A"
-        elif jogador_atual == "A":
-            jogador_atual = "V"
+        if jogador_atual == "O":
+            jogador_atual = "X"
+        elif jogador_atual == "X":
+            jogador_atual = "O"
     else:
         print("Célula ocupada. Escolha outra.")
 if verificar_fim_de_jogo():
